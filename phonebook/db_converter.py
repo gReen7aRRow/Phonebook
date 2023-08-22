9fdcb1efb7b04d5e9dae73726dd7fa169f87000f
gReen7aRRow <f.gusarov@inbox.ru>
2023-08-22 12:05:03 +0300

    create parse_db logic and add any corrections

---
+++ phonebook/db_converter.py
@@ -0,0 +1,15 @@
+import json
+
+
+def from_db() -> dict:
+    with open('phonebook/db.json', 'r') as json_file:
+        data = json.load(json_file)
+
+        return data
+
+
+def to_db(data: dict):
+    with open('phonebook/db.json', 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+
+to_db({'blabla': []})
